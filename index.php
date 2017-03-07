35dd917ac7bfca26bee90a60eb2278e5bc2e25ed
Björn Büttner <Idrinth@users.noreply.github.com>
2017-03-08 00:10:23 +0100

    asjusting start script

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 //consider this an example
 use De\Idrinth\EntityGenerator\EntityGenerator;
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . (is_dir(__DIR__ . DIRECTORY_SEPARATOR . 'vendor')?'':'..' . DIRECTORY_SEPARATOR) . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 $pdo = new PDO('mysql:host=localhost','root','');
 $path = __DIR__ . DIRECTORY_SEPARATOR . 'Classes' . DIRECTORY_SEPARATOR . '{{schema}}';
-$gen = new EntityGenerator($pdo,$path,'Mine',array(__DIR__ . DIRECTORY_SEPARATOR . 'temp'));
+$gen = new EntityGenerator($pdo,$path,'Mine');
 $gen->run(array('information_schema','mysql','performance_schema'));
